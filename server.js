0ffd61cfc5f78afb12a78211ea019d7bf52cf550
vimal kovath <vimal.k@fulfillsolutions.com>
2020-03-21 05:05:17 +0530

    updated server

--- server.js
+++ server.js
@@ -1,32 +1,40 @@
-// var express = require('express');
-// var app = express();
-// var path = require('path');
-// var bodyParser = require('body-parser')
-// var http = require('http').Server(app);
-// const https = require('https');
-// const request = require('request');
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser')
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+const https = require('https');
+const request = require('request');
 
-// var allowCrossDomain = function (req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type');
-//   next();
-// }
+var allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
 
-// app.get('/', (req, res) => res.send('Hello World!'));
-// var port = process.env.PORT || 3005;
-// app.listen(port);
-// console.log('server is running on port ',port);
+  next();
+}
+
+app.use(express.static(__dirname + '/dist'));
+app.use(allowCrossDomain);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 
 
 
-const express = require('express')
-const app = express()
-const port = 3000
 
-var parser = require('simple-excel-to-json')
-var doc = parser.parseXls2Json('/Users/vimalkovath/Downloads/COVID19-India.xlsx');
 
-app.get('/', (req, res) => res.send('Hello World!',doc))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+var port = process.env.PORT || 3005;
+app.listen(port);
+
+console.log('server is running on port ',port);
+
+app.get('*', function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
